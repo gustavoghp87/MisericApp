901fc445488abdd13bede079091c02ff30e1e968
gustavoghp87 <ghp.2120@gmail.com>
2020-01-28 20:36:08 -0300

    Agregado FIREBASE

---
+++ app/src/main/java/com/paquete/misericapp/OpenActivity.java
@@ -0,0 +1,26 @@
+package com.paquete.misericapp;
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Handler;
+
+public class OpenActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 6000;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the MainActivity. */
+                Intent intent = new Intent(OpenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
